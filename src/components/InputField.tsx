cfd393d64e2505ad26352e34c3e60d3a008de015
DOJ <omarjeng999@gmail.com>
2022-09-11 11:49:11 +0000

    create input field, model for todo, and list todos

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useRef } from "react";
+import "./styles.css";
 
-const InputField = () => {
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
+}
+const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
   return (
-    <form className="input">
-      <input type="text" className="input__box" placeholder="Enter a todo" />
-      <button className="input__submit" type="submit">
+    <form
+      className="input"
+      onSubmit={(e) => {
+        handleAdd(e);
+        inputRef.current?.blur();
+      }}
+    >
+      <input
+        ref={inputRef}
+        type="text"
+        className="input__box"
+        placeholder="Enter a todo"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+      />
+      <button className="input_submit" type="submit">
         Go
       </button>
     </form>
