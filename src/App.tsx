ea6ff195b89bd8f2e0871dcf4e1e6adc26345cde
DOJ <omarjeng999@gmail.com>
2022-09-11 11:53:24 +0000

    create a todoList file

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./App.css";
 import InputField from "./components/InputField";
+import TodoList from "./components/TodoList";
 import { Todo } from "./model";
 
 const App: React.FC = () => {
@@ -21,9 +22,7 @@ const App: React.FC = () => {
     <div className="App">
       <span className="heading">DOJ's Todo</span>
       <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
-      {todos.map((todo) => (
-        <p>{todo.todo}</p>
-      ))}
+      <TodoList />
     </div>
   );
 };
