a7e3865aebcb7711edc08922e269978780af963a
DOJ <omarjeng999@gmail.com>
2022-09-11 12:06:45 +0000

    display todo using todoList component

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ const App: React.FC = () => {
     <div className="App">
       <span className="heading">DOJ's Todo</span>
       <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
-      <TodoList />
+      <TodoList todos={todos} setTodos={setTodos} />
     </div>
   );
 };
