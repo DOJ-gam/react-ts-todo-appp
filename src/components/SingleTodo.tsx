6474277f2e648b57a14034d86f72e8135f68fa47
DOJ <omarjeng999@gmail.com>
2022-09-11 12:32:53 +0000

    display singleTodo component inside todoList

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,8 +1,17 @@
 import React from "react";
+import { Todo } from "../model";
 
-interface Props {}
-const SingleTodo: React.FC<Props> = () => {
-  return <div>SingleTodo</div>;
+type Props = {
+  todo: Todo;
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+};
+const SingleTodo: React.FC<Props> = ({ todo, todos, setTodos }) => {
+  return (
+    <form className="todos__single">
+      <span className="todos__single--text">{todo.todo}</span>
+    </form>
+  );
 };
 
 export default SingleTodo;
